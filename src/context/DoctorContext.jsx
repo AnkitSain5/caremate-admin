49a7cb170007c3ebc44cc0b15e05a310b8d8dced
AnkitSain5 <ankitsain2886@gmail.com>
2025-07-06 20:27:28 +0530

    update the backend url

--- src/context/DoctorContext.jsx
+++ src/context/DoctorContext.jsx
@@ -6,7 +6,7 @@ import { toast } from "react-toastify";
 export const DoctorContext = createContext();
 
 const DoctorContextProvider = (props) => {
-  const backendUrl = "http://localhost:4000";
+  const backendUrl = "https://caremate-backend.onrender.com";
   const [dToken, setDToken] = useState(
     localStorage.getItem("dToken") ? localStorage.getItem("dToken") : ""
   );
